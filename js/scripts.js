5a895a4587e7310fab3d3e2378260df96a8ec6f9
Emzzy241 <emzzyoluwole@gmail.com>
2022-07-05 20:52:54 +0100

    done with communicate with pharmacist

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// The first slider created 
+
+var swiper = new Swiper(".offers-slider", {    
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
